f98fb36617635d51bd8a57eeca7467bdb25d60b4
PJY <yongjune28@gamil.com>
2020-05-24 01:02:11 +0900

    MongoDb Test

--- model/PhoneGameModel.js
+++ model/PhoneGameModel.js
@@ -1,152 +1,34 @@
-//var pool = require('./dbConnection');
-var sequelize = require('./dbConnection');
-const fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/hellojs';
+var ObjectID = require('mongodb').ObjectID;
 
-class Phonegames extends Sequelize.Model { }
+var db;
 
-Phonegames.init({
-    id: Sequelize.INTEGER,
-    title: Sequelize.STRING(255),
-    company: Sequelize.STRING(255),
-    platform: Sequelize.STRING(255),
-    synopsis: Sequelize.STRING(1000),
-}, { tableName: 'Phonegames', timestamps: false, sequelize });
-
-class Phonegame_reply extends Sequelize.Model { }
-
-Phonegame_reply.init({
-    phonegame_id: Sequelize.INTEGER,
-    reply: Sequelize.STRING(100),
-
-}, { tableName: 'Phonegame_reply', timestamps: false, sequelize });
-
-class Phonegame {
-    constructor() {
-        try {
-            this.prepareModel();
-        } catch (error) {
-            console.error(error);
-        }
+MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+    if (err) {
+        console.error('MongoDB 연결 실패', err);
+        return;
     }
+    db = client.db();
+});
 
-    async prepareModel() {
-        try {
-            await Phonegames.sync({ force: true });
-            await Phonegame_reply.sync({ force: true });
-            Phonegames.hasMany(Phonegame_reply, { foreignKey: 'phonegame_id' });
-            // sequelize.close();
-        }
-        catch (error) {
-            console.log('country.sync Error ', error);
-        }
-    }
-
-    // -------------------------------------------------------- sequelize 적용 예정 ------------------------------------
-    //리스트 조회
-    getPhoneGameList = async() => {   
-        const sql = 'SELECT * from phonegames'
-        let conn;
-        try {
-            conn = await pool.getConnection();
-            const [rows, metadata] = await conn.query(sql);
-            conn.release();
-            return rows;
-        } catch (error) {
-            console.error(error);
-        } finally {
-            if ( conn ) conn.release();
-        }
-    }
-
-    getPhoneGameDetail = async(gameId) => {
-        const sql = 'SELECT * from phonegames where id = ?';
-        let conn;
-        try {
-            conn = await pool.getConnection();
-            const [rows, metadata] = await conn.query(sql, gameId);
-            conn.release();
-            console.log(rows);
-            return rows[0];
-        } catch (error) {
-            console.error(error);
-        } finally {
-            if ( conn ) conn.release();
-        }
-    }
-
-    addPhoneGame = async(title, company, platform, synopsis) => {
-
-        console.log(title);
-        console.log(company);
-        console.log(platform);
-        console.log(synopsis);
-
-        const data = [title, company, platform, synopsis];
-        const sql = 'insert into phonegames(title,company,platform,synopsis) values(?, ?, ?, ?)';
-        let conn;
-        try {
-            conn = await pool.getConnection();
-            const [rows, metadata] = await conn.query(sql, data);
-            conn.release();
-            console.log('rows',rows);
-            return rows[0];
-        } catch (error) {
-            console.error(error);
-            return -1;
-        } finally {
-            if ( conn ) conn.release();
-        }
-    }
-
-//    getPhoneGameDetail(gameId) {
-//        return new Promise((resolve, reject) => {
-//           for (var game of this.phoneGames ) {
-//                if ( game.id == gameId ) {
-//                    resolve(game);
-//                    return;
-//                }
-//            }
-//            reject({msg:'Can not find game', code:404});
-//        });
-//    }
+class Phonegames { }
 
 
+Phonegames.getPhoneGameList = () => {   
+    return db.collection('movies').find({}).toArray()   
+}
+ 
+Phonegames.getPhoneGameDetail = (id) => {
+    return db.collection('movies').findOne({_id:new ObjectID(id)})
+}
+    
 
-    upPhoneGame = async(gameId, title, company, platform, synopsis) => {
-        const data = [title, company, platform, synopsis, gameId];
-        const sql = 'update phonegames set title = ?, company = ?, platform = ?, synopsis = ? where id = ?';
-        let conn;
-        try {
-            conn = await pool.getConnection();
-            const [rows, metadata] = await conn.query(sql, data);
-            conn.release();
-            console.log('rows',rows);
-            return rows[0];
-        } catch (error) {
-            console.error(error);
-            return -1;
-        } finally {
-            if ( conn ) conn.release();
-        }
-    }
+Phonegames.addPhoneGame = (getTitle, getCompany, getPlatform, getSynopsis) => {
+    const phonegames = db.cllection('phonegames');
 
-    delPhoneGame = async(id) => {
-        
-        const sql = 'delete from phonegames where id = ?';
-        let conn;
-        try {
-            conn = await pool.getConnection();
-            const [rows, metadata] = await conn.query(sql, id);
-            conn.release();
-            console.log('rows',rows);
-            return rows[0];
-        } catch (error) {
-            console.error(error);
-            return -1;
-        } finally {
-            if ( conn ) conn.release();
-        }
-    }
+    // 이게 맞나
+    phonegames.insertOne({title: getTitle, company: getCompany, platform: getPlatform, synopsis: getSynopsis});
 }
 
 
--- model/dbConnection.js
+++ model/dbConnection.js
@@ -1,28 +1,7 @@
-const mysql = require('mysql2');
-const sequelize = new Sequelize('hellojs', 'root', 'cometrue', {
-   dialect: 'mysql', host: 'localhost',
-   pool: {
-   max: 10, // 커넥션 최대 개수
-   min: 0, // 풀에 유지하는 커넥션 최소 개수
-   acquire: 60000, // 커넥션 풀에서 커넥션 얻기 최대 대기 시간(기본 60초)
-   idle: 10000, // 커넥션이 해제되기 전 idle 상태 대기 시간(msec, 기본 10초)
-   evict: 1000, // 커넥션 풀에서 사용하지 않는 커넥션 해제 검사 간격(interval)
-   validate: {} // 커넥션 검사 함수
-   }
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+   const db = client.db('hellojs');
 });
 
-module.exports = sequelize;
-
-/*
-const dbConfig = {
-   host: 'localhost',
-   user: 'root',
-   password: 'cometrue',
-   port: 3306,
-   database: 'hellojs',
-   multipleStatements: true,
-};
-
-const pool = mysql.createPool(dbConfig).promise();
-*/
-
+module.exports = MongoClient;
